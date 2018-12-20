d1010173017a68c4e42bcf2b5d51898acf49f889
Ravi Tiwari <rkrtiwari@gmail.com>
2018-12-20 16:04:07 +0800

    successfully tested on abnormality data

--- alarm_setting_all_functions.py
+++ alarm_setting_all_functions.py
@@ -23,60 +23,58 @@ os.listdir('.')
 ###############################################################################
 # Read the data
 ###############################################################################
-def read_plug_data(i):    
+def read_xl_data(i, name):    
     col_no = range(5,18)
-    plug_data = pd.read_excel('data\\IOT\\shared on 13.12.18\\SMM1 T-1220 Plugging.xlsx',
-                                 usecols = col_no, skiprows = 3, sheet_name=i)
-    return plug_data
+    if name == 'plug_data':
+        fname = 'data\\IOT\\shared on 13.12.18\\SMM1 T-1220 Plugging.xlsx'
+    if name == 'abnormality_data':
+        fname = 'data\\IOT\\shared on 13.12.18\\Abnormality Detection May17 _ Jan18.xlsx'
+        
+    df = pd.read_excel(fname, usecols = col_no, skiprows = 3, sheet_name=i)    
+    return df
 
-plug_data_0 = read_plug_data(0)
-plug_data_1 = read_plug_data(1)
-plug_data_2 = read_plug_data(2)
 
 ###############################################################################
 # clean the data (Remove the non numeric characters)
 ###############################################################################
+#def remove_non_numeric_values(df):
+#    ind = []
+#    for j, n in enumerate(df.iloc[:,1]):
+#        if np.isreal(n):
+#            if not np.isnan(n):
+#                ind.append(j)
+#    cleaned_df = df.loc[ind,:]
+#    return cleaned_df
+
 def remove_non_numeric_values(df):
+    n, _ = df.shape
     ind = []
-    for j, n in enumerate(df.iloc[:,1]):
-        if np.isreal(n):
-            if not np.isnan(n):
-                ind.append(j)
+    for i in range(n):
+        row_vals = df.iloc[i,:].values
+        ind_list = [np.isreal(x) for x in row_vals]
+        ind.append(all(ind_list))
     cleaned_df = df.loc[ind,:]
     return cleaned_df
-    
-cleaned_df_0 = remove_non_numeric_values(plug_data_0)
-cleaned_df_1 = remove_non_numeric_values(plug_data_1)
-cleaned_df_2 = remove_non_numeric_values(plug_data_2)
-###############################################################################
 
-###############################################################################
-# check if all the three df have the same shape and same column names
-###############################################################################
-plug_data_0.shape, plug_data_1.shape, plug_data_2.shape
-cleaned_df_0.shape, cleaned_df_1.shape, cleaned_df_2.shape
-cleaned_df_0.columns
-cleaned_df_1.columns
-cleaned_df_2.columns
-
-###############################################################################
-# merge the three cleaned data frames
-###############################################################################
-frames = [cleaned_df_2, cleaned_df_0, cleaned_df_1]
-cleaned_df = pd.concat(frames, keys=[0,1,2])
 ###############################################################################
 # column description
 ###############################################################################
-def get_col_desc():
-    col_desc = ['EXTRACTION CURRENT LOAD',	'T-8220 PRESS.',
-  'E-8221A/B/C INLET SM', 'T-8220 BTM TEMP.',
-  'E-8223 OUTLET LINE',	'E-8223 OUTLET LINE',
-  'T-8220 BTM TEMP.', 'T-8220 BTM',	
-  'E-8221A INLET SM', 'E-8221A/B/C SM',
-   'E-8221A/B/C INLET OX',	'E-8221A/B INLET LINE']
+def get_col_desc(name):
+    if name == 'abnormality_data':
+        col_desc = ['EXTRACTION CURRENT LOAD',	'T-8220 PRESS.',
+                    'E-8221A/B/C INLET SM', 'T-8220 BTM TEMP.',
+                    'E-8223 OUTLET LINE',	'E-8223 OUTLET LINE',
+                    'T-8220 BTM TEMP.', 'T-8220 BTM',	
+                    'E-8221A INLET SM', 'E-8221A/B/C SM',
+                    'E-8221A/B/C INLET OX',	'E-8221A/B INLET LINE']
+        
+    if name == 'plug_data' :
+        col_desc = ['MAA EXTRACTION LOAD', 'T-1220', 'E-1221A/B INLET LINE', 
+                    'T-1220 BOTTOM LINE',	'E-1223 OUTLET LINE', 'E-1223 OUTLET LINE',
+                    'T-1220 BOTTOM LINE',	'T-1220 BOTTOM', 'E-1221A STEAM', 
+                    'E-1221A/B INLET LINE', 'E-1221A/B INLET LINE', 'E-1221A/B  INLET LINE']
     return col_desc
     
-col_desc = get_col_desc()
 
 ###############################################################################
 # data visualization
@@ -84,8 +82,8 @@ col_desc = get_col_desc()
 def plot_ts(i, df, desc):
     col_name = df.columns[i]
     for j in range(3):
-        x = cleaned_df.loc[j].iloc[:,0]
-        y = cleaned_df.loc[j].iloc[:,i]
+        x = df.loc[j].iloc[:,0]
+        y = df.loc[j].iloc[:,i]
         plt.plot(x, y, color = 'blue') 
     plt.xticks(rotation = 'vertical')
     plt.title(col_name + ' : ' + desc[i-1])    
@@ -95,8 +93,6 @@ def plot_ts(i, df, desc):
     plt.savefig(fname)
     plt.show()
     return
-
-
 
 
 ###############################################################################
@@ -112,8 +108,8 @@ def plot_ts_with_change_point(i, df, desc, change_point):
     for j in range(3):
         ind = change_point[j].index
         x_change = df.loc[j].loc[ind].iloc[:,0]
-        x = cleaned_df.loc[j].iloc[:,0]
-        y = cleaned_df.loc[j].iloc[:,i]
+        x = df.loc[j].iloc[:,0]
+        y = df.loc[j].iloc[:,i]
         plt.plot(x, y, color = 'blue') 
         for t, ct in zip(x_change, change_point[j]):
             if ct == 1:
@@ -129,9 +125,6 @@ def plot_ts_with_change_point(i, df, desc, change_point):
     plt.savefig(fname)
     plt.show()
     return
-
-change_point_series = get_change_point_location(cleaned_df, 90)
-plot_ts_with_change_point(1, cleaned_df, col_desc, change_point_series)
 
 def get_index_to_delete(change_point, margin, df):
     ind_to_delete = []
@@ -157,17 +150,10 @@ def get_merged_change_point(change_point, ind_to_delete):
     merged_index = []
     for j in range(3):
         ind = ind_to_delete[:,0] == j
-        merged_index.append(change_point_series[j].drop(index = ind_to_delete[ind,1]))
+        merged_index.append(change_point[j].drop(index = ind_to_delete[ind,1]))
     
     merged_change_point = pd.concat(merged_index, keys = range(3))
     return merged_change_point 
-
-
-margin = pd.Timedelta('10 days')                
-ind_to_delete = get_index_to_delete(change_point_series, margin, cleaned_df)
-merged_change_point = get_merged_change_point(change_point_series, ind_to_delete)   
-plot_ts_with_change_point(1, cleaned_df, col_desc, merged_change_point)
-
 
 def get_index_for_specified_load(j, merged_change_point, df, margin):
     start_date = df.loc[j].iloc[0,0]
@@ -177,7 +163,7 @@ def get_index_for_specified_load(j, merged_change_point, df, margin):
     for i, item in enumerate(merged_change_point[j]):
         if item == 1:
             st_ind = merged_change_point[j].index[i]
-            start_date = cleaned_df.loc[j].iloc[st_ind,0]
+            start_date = df.loc[j].iloc[st_ind,0]
             try:
                 en_ind = merged_change_point[j].index[i+1]
             except:
@@ -198,10 +184,9 @@ def get_merged_indices(merged_change_point, df, margin = pd.Timedelta('2 days'))
     merged_indices = pd.concat(merged_ind, keys = range(3))
     return merged_indices
 
-merged_indices = get_merged_indices(merged_change_point, cleaned_df)
 
 def plot_values_in_specified_load_and_margin(i, merged_indices, df):
-    title = cleaned_df.columns[i]
+    title = df.columns[i]
     for j in range(3):
         ind = merged_indices[j]
         x = df.loc[j].iloc[:,0] 
@@ -214,8 +199,6 @@ def plot_values_in_specified_load_and_margin(i, merged_indices, df):
     plt.show()
     return
     
-plot_values_in_specified_load_and_margin(7, merged_indices, cleaned_df)
-
 ###############################################################################
 # histogram with alarm limits
 ###############################################################################
@@ -234,7 +217,7 @@ def plot_histogram_with_alarm_limits(i, merged_indices, df):
     plt.axvline(x = mean, color = 'k')
     plt.axvline(x = mean + 3*sd, color = 'red')
     plt.axvline(x = mean - 3*sd, color = 'red')
-    plt.xlabel(cleaned_df.columns[i])
+    plt.xlabel(df.columns[i])
     plt.show()
     return mean, sd    
 
@@ -262,12 +245,7 @@ def plot_alarm_limit_on_ts(i, df, mean, sd):
     plt.show()
     return
 
-for i in range(2,13):
-    mean, sd = plot_histogram_with_alarm_limits(i, merged_indices, cleaned_df)    
-    plot_alarm_limit_on_ts(i, cleaned_df, mean, sd)
-    plot_ts(i, cleaned_df, col_desc)
-
-
+   
 ###############################################################################
 # moving average
 ###############################################################################
@@ -276,21 +254,119 @@ def moving_average(a, n=3) :
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
-i = 3
-for j in range(3):
-    x = cleaned_df.loc[j].iloc[:,0]
-    y = cleaned_df.loc[j].iloc[:,i].copy(deep=True)
-    y = np.array(y, dtype='float')
-    y_mv = moving_average(y, n=5*24*60)
-    plt.plot(x,y, color = 'blue')
-    plt.plot(x[5*24*60-1:],y_mv, color = 'red')
-plt.xticks(rotation = 'vertical')
+def plot_moving_average(i, df, window_size = 1*24*60):
+    for j in range(3):
+        x = df.loc[j].iloc[:,0]
+        y = df.loc[j].iloc[:,i].copy(deep=True)
+        y = np.array(y, dtype='float')
+        y_mv = moving_average(y, n=window_size)
+        plt.plot(x,y, color = 'blue')
+        plt.plot(x[window_size-1:], y_mv, color = 'red')
+    plt.xticks(rotation = 'vertical')
+    plt.show()
+    return
+
+###############################################################################
+# Final Run: Plugged Data
+###############################################################################
+# 1. Read
+plug_data_0 = read_xl_data(0, 'plug_data')
+plug_data_1 = read_xl_data(1, 'plug_data')
+plug_data_2 = read_xl_data(2, 'plug_data')
+
+# 2. Clean
+cleaned_plug_df_0 = remove_non_numeric_values(plug_data_0)
+cleaned_plug_df_1 = remove_non_numeric_values(plug_data_1)
+cleaned_plug_df_2 = remove_non_numeric_values(plug_data_2)
+
+# 3. Merge
+frames = [cleaned_plug_df_2, cleaned_plug_df_0, cleaned_plug_df_1]
+cleaned_plug_df = pd.concat(frames, keys=[0,1,2])
+
+# 4. Column Description
+plug_col_desc = get_col_desc('plug_data')
+
+# 5. Visualize time series
+i = 2
+plot_ts(i, cleaned_plug_df, plug_col_desc)
+
+# 6. Get and visualize change points
+plug_change_points = get_change_point_location(cleaned_plug_df, 90)
+plot_ts_with_change_point(1, cleaned_plug_df, plug_col_desc, plug_change_points)
+
+# 7. merge nearby changepoints
+margin = pd.Timedelta('10 days')                
+ind_to_delete = get_index_to_delete(plug_change_points, margin, cleaned_plug_df)
+plug_merged_change_point = get_merged_change_point(plug_change_points, ind_to_delete)   
+plot_ts_with_change_point(1, cleaned_plug_df, plug_col_desc, plug_merged_change_point)
+plug_merged_indices = get_merged_indices(plug_merged_change_point, cleaned_plug_df)
+
+# 8. visualize the data within given load and with given margin 
+plot_values_in_specified_load_and_margin(3, plug_merged_indices, cleaned_plug_df)
+
+# 9. Get and plot alarm limit
+mean, sd = plot_histogram_with_alarm_limits(i, plug_merged_indices, cleaned_plug_df)
+plot_alarm_limit_on_ts(i, cleaned_plug_df, mean, sd)
+plot_ts(i, cleaned_plug_df, plug_col_desc)
+
+for i in range(2,13):
+    mean, sd = plot_histogram_with_alarm_limits(i, plug_merged_indices, cleaned_plug_df) 
+
+# 10. plot moving average
+plot_moving_average(3, cleaned_plug_df, window_size = 5*24*60)
+
+###############################################################################
+# Final Run: Abnormality Data
+###############################################################################
+# 1. Read
+abnormality_data_0 = read_xl_data(0, 'abnormality_data')
+abnormality_data_1 = read_xl_data(1, 'abnormality_data')
+abnormality_data_2 = read_xl_data(2, 'abnormality_data')
+
+# 2. Clean
+cleaned_abnormal_df_0 = remove_non_numeric_values(abnormality_data_0)
+cleaned_abnormal_df_1 = remove_non_numeric_values(abnormality_data_1)
+cleaned_abnormal_df_2 = remove_non_numeric_values(abnormality_data_2)
+
+# 3. Merge
+frames = [cleaned_abnormal_df_1, cleaned_abnormal_df_0, cleaned_abnormal_df_2]
+cleaned_abnormal_df = pd.concat(frames, keys=[0,1,2])
+
+# 4. Abnormal column description
+abnormal_col_desc = get_col_desc('abnormality_data')
+
+# 5. Visualize time series
+i = 2
+plot_ts(i, cleaned_abnormal_df, abnormal_col_desc)
+
+# 6. Get and visualize change points
+abnormal_change_points = get_change_point_location(cleaned_abnormal_df, 90)
+plot_ts_with_change_point(1, cleaned_abnormal_df, abnormal_col_desc, abnormal_change_points)
+
+# 7. merge nearby changepoints
+margin = pd.Timedelta('10 days')                
+ind_to_delete = get_index_to_delete(abnormal_change_points, margin, cleaned_abnormal_df)
+abnormal_merged_change_point = get_merged_change_point(abnormal_change_points, ind_to_delete)   
+plot_ts_with_change_point(1, cleaned_abnormal_df, abnormal_col_desc, abnormal_merged_change_point)
+abnormal_merged_indices = get_merged_indices(abnormal_merged_change_point, cleaned_abnormal_df)
 
 
+# 8. visualize the data within given load and with given margin 
+plot_values_in_specified_load_and_margin(5, abnormal_merged_indices, cleaned_abnormal_df)
 
+# 9. Get and plot alarm limit
+mean, sd = plot_histogram_with_alarm_limits(i, abnormal_merged_indices, cleaned_abnormal_df)
+plot_alarm_limit_on_ts(i, cleaned_abnormal_df, mean, sd)
+plot_ts(i, cleaned_abnormal_df, abnormal_col_desc)
 
+for i in range(2,13):
+    mean, sd = plot_histogram_with_alarm_limits(i, abnormal_merged_indices, cleaned_abnormal_df) 
+    plot_alarm_limit_on_ts(i, cleaned_abnormal_df, mean, sd)
+    plot_ts(i, cleaned_abnormal_df, abnormal_col_desc)
 
-
-
+# 10. plot moving average
+for i in range(1,13):
+    plot_moving_average(i, cleaned_abnormal_df, window_size = 5*24*60)
+    
 
 
