adc5c7a50f3b385d24ff7a2c1c276389013a12f0
Ravi Tiwari <rkrtiwari@gmail.com>
2018-12-21 16:54:32 +0800

    added moving average plot

--- plotting_functions.py
+++ plotting_functions.py
@@ -42,6 +42,9 @@ def set_y_limit(low, high, ax):
     ax.set_ylim(low, high)
     return ax
 
+def add_y_line(y):
+    ax.axhline(y = y, color = 'k', linewidth = 1, linestyle = '--')
+    return ax
 
 def add_change_points(change_point, df, ax):    
     for j in range(3):
@@ -107,10 +110,14 @@ def get_y_stats(y):
 def plot_histogram_with_alarm_limits(y, mean, sd, ax = None):
     if ax is None:
         f, ax = plt.subplots()
+    hh = round(mean + 3*sd, 2)
+    ll = round(mean - 3*sd, 2)
     sns.distplot(y, bins = 30, color = 'green', vertical = True, ax = ax)
     ax.axhline(y = mean, color = 'k', linestyle = '--')
-    ax.axhline(y = mean + 3*sd, color = 'red', linestyle = '--')
-    ax.axhline(y = mean - 3*sd, color = 'red', linestyle = '--')
+    ax.axhline(y = hh, color = 'red', linestyle = '--')
+    ax.axhline(y = ll, color = 'red', linestyle = '--')
+    ax.text(0.1, 0.9, 'HH: ' + str(hh), transform=ax.transAxes)
+    ax.text(0.1, 0.8, 'LL: ' + str(ll), transform=ax.transAxes)
     return ax
 
 def plot_alarm_limit_on_ts(x, y, mean, sd, ax = None):
@@ -139,10 +146,11 @@ def plot_alarm_limit_on_ts(x, y, mean, sd, ax = None):
 
 i = 2
 start_date = '2014-04-16 00:00:00'
-end_date = '2014-12-16 00:00:00'
+end_date = '2018-12-07 00:00:00'
 margin = pd.Timedelta('10 days')
 
 ax = plot_ts(1, cleaned_abnormal_df)
+ax = add_y_line(90)
 #ax = set_y_limit(86, 93, ax)
 
 #ax = add_change_points(abnormal_change_points, cleaned_abnormal_df, ax)
@@ -151,7 +159,7 @@ ax = set_x_limit(start_date, end_date, ax)
 ax = set_y_limit(70, 110, ax)
 ax = plot_values_in_specified_load_and_margin(i, abnormal_merged_indices, cleaned_abnormal_df, ax)
 #ax = set_x_limit(start_date, end_date, ax)
-ax = set_y_limit(180, 280, ax)
+#ax = set_y_limit(180, 280, ax)
 
 dates = [start_date, end_date]
 x, y, y_clean = subset_data_for_alarm_limit_setting(i, dates, abnormal_merged_indices, cleaned_abnormal_df)
@@ -161,12 +169,32 @@ plot_alarm_limit_on_ts(x, y, mean, sd, ax1)
 plot_histogram_with_alarm_limits(y_clean, mean, sd, ax2)
 
 
+###############################################################################
+# moving average
+###############################################################################
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
+def plot_moving_average(i, df, window_size = 1*24*60, ax = None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    for j in range(3):
+        x = df.loc[j].iloc[:,0]
+        y = df.loc[j].iloc[:,i].copy(deep=True)
+        y = np.array(y, dtype='float')
+        y_mv = moving_average(y, n=window_size)
+        ax.plot(x[window_size-1:], y_mv, color = 'red')
+    
+    ax.tick_params(axis='x', rotation=90)
+    return ax
 
-
-
-
-
+ax = plot_ts(2, cleaned_abnormal_df)
+ax = plot_moving_average(2, cleaned_abnormal_df, window_size = 5*24*60, ax = ax)
+ax = set_x_limit(start_date, end_date, ax)
+ax = set_y_limit(200, 260, ax)
 
 
 ###############################################################################
