f2afc4711941994aea6293b59e5329d21a0ac70e
unknown <rkrtiwari@gmail.com>
2019-02-18 10:32:41 +0800

    organized file opening codes into function

--- open_files.py
+++ open_files.py
@@ -9,13 +9,16 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 2.workstation
+###############################################################################
+# Set up the working directory
+###############################################################################
 os.chdir('D:\\sumitomo\\code\\alarm-limit')
 os.listdir('.')
 
 
-
+###############################################################################
 # function to get file size in human readable format
+###############################################################################
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
@@ -24,53 +27,104 @@ def sizeof_fmt(num, suffix='B'):
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
-# get all file name
-folder = 'D:\\sumitomo\data'
-fnames = os.listdir(folder)
+###############################################################################
+# Organizing the files based on equipment
+###############################################################################
+def get_file_list_in_a_folder(folder):
+    fnames = os.listdir(folder)
+    return fnames
 
-for fname in fnames:
-    if fname == 'New folder':
-        pass
-    else:
+
+def organize_the_file_names_in_cat(fnames):
+    cat_dict = {}
+    for fname in fnames:
+        cat = fname.split()[1]
+        try:
+            cat_dict[cat].append(fname)
+        except:
+            cat_dict[cat] = []
+            cat_dict[cat].append(fname)
+    return cat_dict
+        
+###############################################################################
+# get the file sizes
+###############################################################################
+def print_file_sizes(cat_dict, folder):
+    for fnames in cat_dict.values():
+        print('\n')
+        for fname in fnames:
+            full_path = os.path.join(folder, fname)
+            st = os.stat(full_path)
+            print(fname  +  ' : ' + sizeof_fmt(st.st_size))
+    return
+            
+
+
+
+folder1 = 'D:\\sumitomo\data'
+fnames1 = get_file_list_in_a_folder(folder1)
+
+folder2 = 'D:\\sumitomo\\data\\New folder'
+fnames2 = os.listdir(folder2)
+
+cat_dict1 = organize_the_file_names_in_cat(fnames1)
+cat_dict2 = organize_the_file_names_in_cat(fnames2) 
+
+print_file_sizes(cat_dict1, folder1)
+print_file_sizes(cat_dict2, folder2)    
+
+
+###############################################################################
+# open a single file
+###############################################################################
+def open_a_single_file(fname, folder):
+    full_path = os.path.join(folder, fname)
+    st = os.stat(full_path)    
+    print(fname + ' : ' + sizeof_fmt(st.st_size))
+    os.startfile(full_path)
+    return
+
+fname = 'SMM1 T-1330 temp data1.xlsx' 
+
+open_a_single_file(fname, folder1)
+
+###############################################################################
+# Open all files related to a given equipment
+###############################################################################
+
+def open_all_files_of_a_tag(tag, cat_dict, folder):
+    for fname in cat_dict[tag]:
         print(fname)
         full_path = os.path.join(folder, fname)
-        st = os.stat(full_path)
-        print('   :' + sizeof_fmt(st.st_size))
-        
+        os.startfile(full_path)
+    return
+    
+
+folder1 = 'D:\\sumitomo\data'
+tag = 'T-1330'
+open_all_files_of_a_tag(tag, cat_dict1, folder1)
 
 ###############################################################################
-# open a file
+# Open all files one by one for inspection
 ###############################################################################
-full_path = os.path.join(folder, fnames[2])
-st = os.stat(full_path)
-sizeof_fmt(st.st_size)
-os.startfile(full_path)
-        
-###############################################################################
- 
-
-###############################################################################
-# open files one by one
-###############################################################################
-def open_file_one_by_one(fnames, folder):
+def open_all_files_in_a_folder(folder):
+    fnames = os.listdir(folder)
     for fname in fnames:
         full_path = os.path.join(folder, fname)
-        try:
-            os.startfile(full_path)
-            yield
-        except:
-            pass
-        
-# get the list of all the files
-folder = 'D:\\sumitomo\data'
-fnames = os.listdir(folder)
-                
-fopen = open_file_one_by_one(fnames, folder)
+        st = os.stat(full_path)
+        print(fname  +  ' : ' + sizeof_fmt(st.st_size))
+        print('want to open: y or n')
+        x = input()
+        if x == 'y':            
+            try:
+                os.startfile(full_path)
+            except:
+                pass
+    return
 
-try:
-    next(fopen)
-except:
-    pass
+folder = 'D:\\sumitomo\data'
+open_all_files_in_a_folder(folder)
+
 
 
 ###############################################################################
@@ -131,35 +185,10 @@ full_path = os.path.join(folder, fname[0])
 os.startfile(full_path)
 
 
-###############################################################################
-# next organizing the data (equipment wise)
-###############################################################################
-folder1 = 'D:\\sumitomo\data'
-fnames1 = os.listdir(folder)
 
-folder2 = 'D:\\sumitomo\\data\\New folder'
-fnames2 = os.listdir(folder)
-
-cat_dict = {}
-for fname in fnames1:
-    cat = fname.split()[1]
-    try:
-        cat_dict[cat].append(fname)
-    except:
-        cat_dict[cat] = []
-        cat_dict[cat].append(fname)
-        
-for key in cat_dict.keys():
-    print(key)
-    print(cat_dict[key])        
-
-cat_dict['T-1220']   
- 
-
-    
    
 
-fnames1
+
 
 
 
